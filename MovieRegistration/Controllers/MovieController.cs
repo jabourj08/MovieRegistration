079049cbd2e694027562b9ce0ca1d9827f368414
Josh <jabourjosh@gmail.com>
2020-08-24 08:57:16 -0400

    first commit for MovieRegistration

---
+++ MovieRegistration/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieRegistration.Models;
+
+namespace MovieRegistration.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult MovieRegistration()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayMovie(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                //movie.Actors.Add()
+                return View(movie);
+            }
+            else
+            {
+                return View("MovieRegistration", movie);
+            }
+        }
+    }
+}
---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public List<string> Actors { get; set; }
+        public string ActorsString
+        {
+            get
+            {
+                return string.Join(",", Actors);
+            }
+            set
+            {
+                Actors = value.Split(new char[] { ',' }).Select(x => x.Trim()).ToList();
+            }
+        }
+        public List<string> Directors { get; set; }
+        public string DirectorsString
+        {
+            get
+            {
+                return string.Join(",", Directors);
+            }
+            set
+            {
+                Directors = value.Split(new char[] { ',' }).Select(x => x.Trim()).ToList();
+            }
+        }
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(int ID, string Title, string Genre, int Year, List<string> Actors, List<string> Directors)
+        {
+            this.ID = ID;
+            this.Title = Title;
+            this.Genre = Genre;
+            this.Year = Year;
+            this.Actors = Actors;
+            this.Directors = Directors;
+        }
+    }
+}
